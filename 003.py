bf2a79e4260e1fe1e507cfa9346313fd1d0130d9
unknown <edivan220479@gmail.com>
2024-04-09 17:07:25 -0300

    commi inicial

---
+++ 003.py
@@ -0,0 +1,24 @@
+'''num1 = 5
+num2 = 3.5
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(10 / 3)
+print(10 // 3)
+print(10 % 3)
+print(2 ** 3)
+print(3+5*7+3)
+print((3+5)*(7+3))
+
+print(abs(-15)) #asoluto
+print(pow(3,3)) #esponiciação
+print(max(5,9,78,66,4))#maior
+print(min(3,8,-88,1))#menor
+print(round(8.3))#aredondamento'''
+
+import math
+print(math.floor(8.99))#aredonda para baixo
+print(math.ceil(8.01))#aredonda para cima
+print(math.sqrt(9))#raiz quadrada
+
